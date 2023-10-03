bcdc6d0a037aa6f5b27b8ed525f89ca126df8ca6
Bill DeRouchey <billder@users.noreply.github.com>
2023-10-03 13:25:15 -0700

    Create PCCImage.js

---
+++ components/PCCImage.js
@@ -0,0 +1,7 @@
+export default function PCCImage() {
+   return (
+      <div className="pcc-image">
+         <a href="https://www.credly.com/badges/e68d4a86-1115-4448-afcf-ae254111a2d7/" target="_new"><img src="pcc.png" class="center"/></a>
+      </div>
+   )
+}
