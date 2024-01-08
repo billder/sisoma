1118f16f6014d7edcc8e83c7b908281189258763
Bill DeRouchey <billder@users.noreply.github.com>
2024-01-08 12:30:47 -0800

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Header1 from '@components/Header1'
+import Header2 from '@components/Header2'
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <meta http-equiv="refresh" content="0; URL=https://www.sisoma.com/" />
+      </Head>
+    </div>
+  )
+}
